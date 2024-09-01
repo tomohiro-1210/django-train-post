1e1a84b7e6b1c73dc09af26ba59b6bda72c3590c
tomohiro_0225 <express22212270@gmail.com>
2024-09-01 10:29:30 +0900

    新規登録で重複があったときのエラー設定

--- trainpostapp/views.py
+++ trainpostapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Useモデルの読み込み(Django標準搭載)
 from django.contrib.auth.models import User
+# Errorの読み込み
+from django.db import IntegrityError
 
 # Create your views here.
 def index(request):
@@ -15,8 +17,13 @@ def signupfunc(request):
       username = request.POST['username']
       email = request.POST['email']
       password = request.POST['password']
-      # Userモデルにデータを登録する
-      user = User.objects.create_user(username, email, password)
+      try:
+         # Userモデルにデータを登録する
+         user = User.objects.create_user(username, email, password)
+      except IntegrityError:
+         error = "入力された名前・メールアドレスは既に使われております。"
+         data = {'error': error}
+         return render(request, template, data)
    else:
       print('リダイレクト')
    
